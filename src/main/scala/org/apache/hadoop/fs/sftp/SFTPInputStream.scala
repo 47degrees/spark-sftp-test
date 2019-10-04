3856fc78c8aaa43825f650b67b4e043a08237854
John <Juan Manuel Mndez Rey vejeta@gmail.com>
2019-10-04 02:03:25 +0200

    Remove warnings

--- src/main/scala/org/apache/hadoop/fs/sftp/SFTPInputStream.scala
+++ src/main/scala/org/apache/hadoop/fs/sftp/SFTPInputStream.scala
@@ -46,10 +46,10 @@ class SFTPInputStream private[sftp] (
   if (wrappedStream == null) throw new IllegalArgumentException(SFTPInputStream.E_NULL_INPUTSTREAM)
   if (channel == null || !channel.isConnected)
     throw new IllegalArgumentException(SFTPInputStream.E_CLIENT_NULL)
-  this.pos = 0
-  this.closed = false
   private var closed = false
   private var pos    = 0L
+  this.pos = 0
+  this.closed = false
   // We don't support seek unless the current position is same as the desired position.
   @throws[IOException]
   override def seek(position: Long): Unit = { // If seek is to the current pos, then simply return. This logic was added so that the seek call in
